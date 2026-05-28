b46d8bcbeba5f89a8373949cd6d7daef4633185b
Abhinavgupta2025 <152771720+Abhinavgupta2025@users.noreply.github.com>
2026-05-28 23:22:34 +0530

    Time: 69 ms (61.38%), Space: 24.3 MB (68.38%)

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        bitset<27> A[2];
-
-        for (auto& ch : word) {
-            int i = ch & 31;
-            int Case = (ch >> 5) & 1;
-
-            A[Case].set(i, !(Case & A[0][i]));
+         int n = word.length();
+        vector<int> arr(26,-1);
+        for(int i=0;i<word.length();i++){
+            if(word[i]>='A' && word[i]<='Z' && arr[word[i]-'A'] == -1){
+                arr[word[i]-'A'] = i;
+            }
         }
-
-        return (A[0] & A[1]).count();
+        vector<int> arr2(26,0);
+        for(int i=0;i<n;i++){
+            if(word[i]>='a' && word[i]<='z'){
+                if(arr[word[i]-'a']>i && arr[word[i]-'a']!=-1) arr2[word[i]-'a'] = 1;
+                else arr2[word[i]-'a'] = -1;
+            }
+        }
+        int ans = 0;
+        for(int i=0;i<26;i++){
+            if(arr2[i]==1) ans++;
+        }
+        return ans;
     }
 };
