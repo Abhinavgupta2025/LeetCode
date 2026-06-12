9f2ee05047bb0be06624777e00288119313b1125
Abhinavgupta2025 <152771720+Abhinavgupta2025@users.noreply.github.com>
2026-06-12 23:11:02 +0530

    Time: 0 ms (100%), Space: 11.7 MB (10.99%)

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+            int n = nums.size();
+            int left = 0;
+            int right = n-1;
+            int mid = 0;
+            while(mid<=right){
+                if(nums[mid]==0){
+                    swap(nums[mid],nums[left]);
+                    left++;
+                    mid++;
+                }
+                else if(nums[mid] == 2){
+                    swap(nums[mid],nums[right]);
+                    right--;
+                }
+                else mid++;
+            }
+            return;
+    }
+};
