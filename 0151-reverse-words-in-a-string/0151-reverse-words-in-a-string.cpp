2d973da694bc82030f817208e9130ea43d6a7f00
Abhinavgupta2025 <152771720+Abhinavgupta2025@users.noreply.github.com>
2026-06-16 18:02:17 +0530

    Time: 0 ms (100%), Space: 9.9 MB (87.87%)

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        reverse(s.begin(),s.end());
+        int left = 0;
+        int right = 0;
+        int start = 0;
+        while(right<n){
+            while(right<n && s[right]==32){
+                right++;
+            }
+            if(right==n) break;
+            while(right<n && s[right]!=32){
+                s[left++] = s[right++];
+            }
+            reverse(s.begin()+start,s.begin()+left);
+            s[left] = ' ';
+            left++;
+            start = left;
+            right++;
+
+        }
+        s.resize(left-1);
+        return s;
+    }
+};
