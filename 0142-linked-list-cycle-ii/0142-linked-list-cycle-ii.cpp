1736a9b6bb84ec20bb4c7da6b1a11b948814e5b8
Abhinavgupta2025 <152771720+Abhinavgupta2025@users.noreply.github.com>
2026-06-17 16:26:14 +0530

    Time: 5 ms (83.44%), Space: 11.3 MB (85.63%)

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+            if(!head || !head->next) return NULL;
+            ListNode* slow = head;
+            ListNode* fast = head;
+            bool flag = false;
+            while(fast && fast->next){
+                slow = slow->next;
+                fast = fast->next->next;
+                if(slow == fast){
+                    flag = true;
+                    break;
+                }
+            }
+            if(!flag) return NULL;
+            slow = head;
+            while(slow != fast){
+                slow = slow->next;
+                fast = fast->next;
+            }
+            return slow;
+    }
+};
