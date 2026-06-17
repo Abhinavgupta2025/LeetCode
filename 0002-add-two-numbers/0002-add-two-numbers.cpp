5d8620f39ed834a3d8681f3c41ab7b729d0ae69e
Abhinavgupta2025 <152771720+Abhinavgupta2025@users.noreply.github.com>
2026-06-17 18:21:56 +0530

    Time: 3 ms (34.76%), Space: 77 MB (74.27%)

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -1,37 +1,25 @@
+
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
             ListNode* dummy = new ListNode(-1);
-            ListNode* temp  = dummy;
+            ListNode* temp = dummy;
             int carry = 0;
-            while(l1 && l2){
-                int val = l1->val + l2->val + carry;
+            while(l1 || l2 || carry){
+                int val = 0;
+                if(l1!=NULL){
+                    val += l1->val;
+                }
+                if(l2!=NULL){
+                    val += l2->val;
+                }
+                val += carry;
                 ListNode* node = new ListNode(val%10);
                 carry = val/10;
                 temp->next = node;
                 temp = temp->next;
-                l1 = l1->next;
-                l2 = l2->next;
-            }
-            while(l1){
-                int val = l1->val + carry;
-                ListNode* node = new ListNode(val%10);
-                carry = val/10;
-                temp->next = node;
-                temp = temp->next;
-                l1 = l1->next;
-            }
-            while(l2){
-                int val = l2->val + carry;
-                ListNode* node = new ListNode(val%10);
-                carry = val/10;
-                temp->next = node;
-                temp = temp->next;
-                l2 = l2->next;
-            }
-            if(carry){
-                ListNode* node = new ListNode(carry);
-                temp->next = node;
+                if(l1)l1 = l1->next;
+                if(l2)l2 = l2->next;
             }
             return dummy->next;
     }
