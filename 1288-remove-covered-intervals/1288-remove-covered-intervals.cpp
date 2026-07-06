e852102c363ffd890f858aec17ff55739ccb676b
Abhinavgupta2025 <152771720+Abhinavgupta2025@users.noreply.github.com>
2026-07-06 22:35:29 +0530

    Time: 11 ms (13.38%), Space: 15.8 MB (14.83%)

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    static bool comp(vector<int>& a, vector<int>& b) {
-        if (a[0] == b[0])
-            return a[1] > b[1];   
-        return a[0] < b[0];      
-    }
+    
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-         sort(intervals.begin(),intervals.end(),comp);
+         sort(intervals.begin(),intervals.end(),[](vector<int>& a,vector<int>& b){
+             if(a[0]==b[0]){
+                return a[1]>b[1];
+             }
+             return a[0]<b[0];
+         });
          vector<vector<int>> ans;
          for(auto it : intervals){
              if(ans.size()==0 || (it[0]>=ans.back()[0] && it[1]>ans.back()[1])) ans.push_back(it);
