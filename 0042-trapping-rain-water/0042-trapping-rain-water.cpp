a9ea27082560cbf19bdf1c0e33c019d2d44f7e1a
Abhinavgupta2025 <152771720+Abhinavgupta2025@users.noreply.github.com>
2026-07-08 09:31:44 +0530

    Time: 3 ms (16.87%), Space: 25.9 MB (78.46%)

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,20 +2,29 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        vector<int> pre(n,-1);
-        vector<int> suff(n,-1);
-        pre[0] = height[0];
-        suff[n-1] = height[n-1];
-        for(int i=1;i<n;i++){
-            pre[i] = max(height[i],pre[i-1]);
-        }
-        for(int i=n-2;i>=0;i--){
-            suff[i] = max(height[i],suff[i+1]);
-        }
+        int left = 0;
+        int leftMax = -1;
+        int right = n-1;
+        int rightMax = -1;
         int count = 0;
-        for(int i=0;i<n;i++){
-            if(height[i]<pre[i] && height[i]<suff[i]){
-                count += ((min(pre[i],suff[i])) - height[i]);
+        while(left<right){
+            if(height[left]<height[right]){
+                if(height[left]<leftMax){
+                    count += leftMax-height[left];
+                }
+                else{
+                    leftMax = height[left];
+                }
+                left++;
+            }
+            else{
+                if(height[right]<rightMax){
+                    count += rightMax-height[right];
+                }
+                else{
+                    rightMax = height[right];
+                }
+                right--;
             }
         }
         return count;
